9dabe888bd6170ece0b19683acad34a77a9a55c2
asgiers <alinesgiers@gmail.com>
2024-01-23 13:52:32 -0300

    Exercício 10 - conceitos básicos

---
+++ exercicio10.py
@@ -0,0 +1,15 @@
+#Faça um Programa que utilize 4 variáveis como preferir e no final print
+#uma mensagem amigável utilizando as variáveis criadas.
+#Exemplos de variáveis: nome, idade, lugar, profissão ....
+#Exemplo de retorno: Olá Maria, prazer te conhecer. Sou de São Paulo
+#também e estou migrando de área.
+#Lembrando que para o retorno vamos usar print com as variáveis
+#criadas e este texto é somente um exemplo, utilizem a criatividade.
+
+nome = 'Aline'
+linguagem = 'Python'
+fator1 = 'computador'
+fator2 = 'internet'
+
+print(f'Olá {nome}, seja bem-vinda à WoMakers!\nIniciarás o curso utilizando a linguagem {linguagem}.\nPara participar do bootcamp é necessário que tenhas um {fator1} e acesso à {fator2}.\nVenha programar um novo futuro!')
+
